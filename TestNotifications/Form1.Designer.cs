28e0af0d2381ebcf11360832d5262fe7db8892bc
JackBossi <giacomo.bossi.14-2@accademiadigitaleliguria.it>
2025-12-03 14:55:14 +0100

    testDev

---
+++ TestNotifications/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace TestNotifications
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            toastNotificationsManager1 = new DevExpress.XtraBars.ToastNotifications.ToastNotificationsManager(components);
+            button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)toastNotificationsManager1).BeginInit();
+            SuspendLayout();
+            // 
+            // toastNotificationsManager1
+            // 
+            toastNotificationsManager1.ApplicationId = "5a52ec02-f13e-4658-8f69-046dff162e28";
+            toastNotificationsManager1.Notifications.AddRange(new DevExpress.XtraBars.ToastNotifications.IToastNotificationProperties[] { new DevExpress.XtraBars.ToastNotifications.ToastNotification("1a563ef4-a6c7-4eb1-bc1f-05275dc13425", (System.Drawing.Image)resources.GetObject("toastNotificationsManager1.Notifications"), "UNODUETRE", "OKOK", "KKK", DevExpress.XtraBars.ToastNotifications.ToastNotificationTemplate.Text01) });
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(200, -14);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(211, 141);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(570, 193);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)toastNotificationsManager1).EndInit();
+            ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.ToastNotifications.ToastNotificationsManager toastNotificationsManager1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TestNotifications/Form1.cs
@@ -0,0 +1,47 @@
+﻿
+using DevExpress.XtraBars.ToastNotifications;
+
+using System;
+
+
+namespace TestNotifications
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        ToastNotificationsManager manager;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            var manager = new ToastNotificationsManager();
+            manager.ApplicationId = "TestApplicativo"; // Unique ID
+
+            // Add a notification.
+            var note = new ToastNotification();
+            note.Template = ToastNotificationTemplate.Text02;
+            note.Header = "Funziona";
+            note.Body = "DEBUG.";
+            note.ID = Guid.NewGuid().ToString(); // A unique identifier
+            manager.Notifications.Add(note);
+
+            // Display the notification.
+            manager.ShowNotification(note.ID);
+
+
+            toastNotificationsManager1.ShowNotification(toastNotificationsManager1.Notifications[0]);
+            toastNotificationsManager1.ShowNotification("3b7fcd8b-a1e0-4ff5-83ce-023cdf6be24b");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
